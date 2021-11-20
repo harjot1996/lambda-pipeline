2ccb908f730964cb2e31fb4e9e0ec6f1bcd6ee81
harjot1996 <harjot1996@users.noreply.github.com>
2021-11-20 05:27:51 -0500

    changes

--- search-photos.py
+++ search-photos.py
@@ -2,17 +2,7 @@ import json
 import boto3
 import urllib3
 
-def get_image_urls(images):
-    base_url = 'https://b2-foto.s3.us-east-1.amazonaws.com/'
-    results = []
-    for name, labels in images:
-        results.append({
-            "url": base_url+name,
-            "labels": labels
-        })
-    return {"results": results}
-
-def fetch_elastic_tags(keyword):
+def fetch_results_from_es(keyword):
     url = "https://vpc-photos-zvgzdxzcebvr2rrlzjki5wwchm.us-east-1.es.amazonaws.com/photos/_search?q="+keyword
     awsauth = 'user:Pa$$word2020'
     http = urllib3.PoolManager()
@@ -20,51 +10,64 @@ def fetch_elastic_tags(keyword):
     r = http.request('GET', url, headers=headers)
     return json.loads(r.data.decode('utf8'))
 
-def search_labels(one, two, three):
+def search_photos(one, two, three):
     results = []
-    results.append(fetch_elastic_tags(one))
+    results.append(fetch_results_from_es(one))
     if two is not None:
-        results.append(fetch_elastic_tags(two))
+        results.append(fetch_results_from_es(two))
     if three is not None:
-        results.append(fetch_elastic_tags(three))
+        results.append(fetch_results_from_es(three))
    
     output = []
     unique_keys = set()
     for r in results:
-        if 'hits' not in r:
-            continue
-        for val in r['hits']['hits']:
-            key = val['_source']['objectKey']
-            if key in unique_keys:
-                continue
-            labels = val['_source']['labels']
-            output.append((key, labels))
-            unique_keys.add(key)
+        if 'hits' in r:
+             for val in r['hits']['hits']:
+                key = val['_source']['objectKey']
+                if key not in unique_keys:
+                    labels = val['_source']['labels']
+                    output.append((key, labels))
+                    unique_keys.add(key)
     return output
    
-
+def generate_response(images):
+    bucket_url = 'https://b2-foto.s3.us-east-1.amazonaws.com/'
+    results = []
+    for img in images:
+        name, labels = img
+        results.append({
+            "url": bucket_url+name,
+            "labels": labels
+        })
+    return {
+        "results": results
+    }
 
 def lambda_handler(event, context):
     client = boto3.client('lex-runtime', region_name='us-east-1')
 
-    lex_response = client.post_text(
+    response_lex = client.post_text(
     botName='photo_bot',
     botAlias="bot",
     userId="rand_str",
     inputText = event["search_query"])
    
-    dialog_state = lex_response["dialogState"]
    
-    if dialog_state == 'ReadyForFulfillment' and 'slots' in lex_response:
+    dialog_state = response_lex["dialogState"]
+   
+    if dialog_state == 'ReadyForFulfillment' and 'slots' in response_lex:
         pass
     else:
         return {
         'statusCode': 404,
-        'body': json.dumps({"message": "Try again"})
+        'body': json.dumps({"message": "Elicit slots again"})
     }
    
-    one = lex_response["slots"]["one"]
-    two = lex_response["slots"]["two"]
-    three = lex_response["slots"]["three"]
-    image_links = search_labels(one, two, three)
-    return get_image_urls(image_links)
+    one = response_lex["slots"]["one"]
+    two = response_lex["slots"]["two"]
+    three = response_lex["slots"]["three"]
+   
+   
+    photos = search_photos(one, two, three)
+   
+    return generate_response(photos)
